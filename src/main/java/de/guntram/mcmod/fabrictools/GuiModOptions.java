8cde6715e23ca785d4499b77e45831777171f172
Guntram Blohm <gbl@guntram.de>
2020-09-01 13:55:50 +0200

    add color selector

--- src/main/java/de/guntram/mcmod/fabrictools/GuiModOptions.java
+++ src/main/java/de/guntram/mcmod/fabrictools/GuiModOptions.java
@@ -11,6 +11,9 @@ import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.AbstractButtonWidget;
 import static net.minecraft.client.gui.widget.AbstractButtonWidget.WIDGETS_LOCATION;
 import net.minecraft.client.gui.widget.TextFieldWidget;
+import net.minecraft.client.render.BufferBuilder;
+import net.minecraft.client.render.Tessellator;
+import net.minecraft.client.render.VertexFormats;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.StringRenderable;
@@ -18,8 +21,10 @@ import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Matrix4f;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.lwjgl.opengl.GL11;
 
 public class GuiModOptions extends Screen implements Supplier<Screen> {
     
@@ -50,6 +55,8 @@ public class GuiModOptions extends Screen implements Supplier<Screen> {
     private static final Text trueText = new TranslatableText("de.guntram.mcmod.fabrictools.true").formatted(Formatting.GREEN);
     private static final Text falseText = new TranslatableText("de.guntram.mcmod.fabrictools.false").formatted(Formatting.RED);
     
+    private ColorSelector colorSelector;
+    
     @SuppressWarnings("OverridableMethodCallInConstructor")
     public GuiModOptions(Screen parent, String modName, ModConfigurationHandler confHandler) {
         super(new LiteralText(modName));
@@ -59,6 +66,7 @@ public class GuiModOptions extends Screen implements Supplier<Screen> {
         this.screenTitle=modName+" Configuration";
         this.options=handler.getConfig().getKeys();
         this.LOGGER=LogManager.getLogger();
+        this.colorSelector = new ColorSelector(new LiteralText("Color"));
     }
     
     @Override
@@ -67,6 +75,10 @@ public class GuiModOptions extends Screen implements Supplier<Screen> {
         if (buttonWidth > 200) {
             buttonWidth = 200;
         }
+
+        /* This button should always be in first position so it gets clicks
+         * before any scrolled-out-of-visibility buttons do.
+         */
         this.addButton(new AbstractButtonWidget(this.width / 2 - 100, this.height - 27, 200, BUTTONHEIGHT, new TranslatableText("gui.done")) {
             @Override
             public void onClick(double x, double y) {
@@ -81,7 +93,7 @@ public class GuiModOptions extends Screen implements Supplier<Screen> {
                 client.openScreen(parent);
             }
         });
-        
+
         int y=50-LINEHEIGHT;
         for (String text: options) {
             y+=LINEHEIGHT;
@@ -159,6 +171,19 @@ public class GuiModOptions extends Screen implements Supplier<Screen> {
                 });
                 ((TextFieldWidget) element).setMaxLength(120);
                 element.changeFocus(false);
+            } else if (value instanceof Integer && (Integer) handler.getConfig().getMin(text) == 0 && (Integer) handler.getConfig().getMax(text) == 15) {
+                // ok, this is quite hacky; assume selections from 0..15 are color indexes ...
+                element=this.addButton(new AbstractButtonWidget(this.width/2+10, y, buttonWidth, BUTTONHEIGHT, null) {
+                            @Override
+                            public void onClick(double x, double y) {
+                                enableColorSelector(text, this);
+                            }
+                            @Override
+                            public void setMessage(Text ignored) {
+                                super.setMessage(new TranslatableText("de.guntram.mcmod.fabrictools.color").formatted(Formatting.byColorIndex((Integer)handler.getConfig().getValue(text))));
+                            }
+                });
+                element.setMessage(null);
             } else if (value instanceof Integer || value instanceof Float || value instanceof Double) {
                 element=this.addButton(new GuiSlider(this.width/2+10, y, handler.getConfig(), text));
             } else {
@@ -178,6 +203,9 @@ public class GuiModOptions extends Screen implements Supplier<Screen> {
         if (maxScroll < 0) {
             maxScroll = 0;
         }
+        
+        colorSelector.init();
+        this.addButton(colorSelector);
     }
     
     @Override
@@ -206,33 +234,43 @@ public class GuiModOptions extends Screen implements Supplier<Screen> {
         }
 */
 
-
-        int y = TOP_BAR_SIZE + LINEHEIGHT/2 - scrollAmount;
-        for (int i=1; i<this.buttons.size(); i+=2) {
-            if (y > TOP_BAR_SIZE - LINEHEIGHT/2 && y < height - BOTTOM_BAR_SIZE) {
-                textRenderer.draw(stack, new TranslatableText(options.get(i/2)), this.width / 2 -155, y+4, 0xffffff);
-                ((AbstractButtonWidget)this.buttons.get(i)).y = y;                                          // config elem
-                ((AbstractButtonWidget)this.buttons.get(i)).render(stack, mouseX, mouseY, partialTicks);
-                ((AbstractButtonWidget)this.buttons.get(i+1)).y = y;                                        // reset button
-                ((AbstractButtonWidget)this.buttons.get(i+1)).render(stack, mouseX, mouseY, partialTicks);
-            }
-            y += LINEHEIGHT;
-        }
-        
-        y = TOP_BAR_SIZE - scrollAmount;
-        for (String text: options) {
-            if (mouseX>this.width/2-155 && mouseX<this.width/2 && mouseY>y && mouseY<y+BUTTONHEIGHT) {
-                StringRenderable tooltip=new TranslatableText(handler.getConfig().getTooltip(text));
-                if (textRenderer.getWidth(tooltip)<=250) {
-                    renderTooltip(stack, tooltip, 0, mouseY);
-                } else {
-                    List<StringRenderable> lines = textRenderer.wrapLines(tooltip, 250);
-                    renderTooltip(stack, lines, mouseX, mouseY);
+        if (!colorSelector.visible) {
+            int y = TOP_BAR_SIZE + LINEHEIGHT/2 - scrollAmount;
+            for (int i=1; i<this.buttons.size(); i+=2) {
+                if (y > TOP_BAR_SIZE - LINEHEIGHT/2 && y < height - BOTTOM_BAR_SIZE) {
+                    textRenderer.draw(stack, new TranslatableText(options.get(i/2)), this.width / 2 -155, y+4, 0xffffff);
+                    ((AbstractButtonWidget)this.buttons.get(i)).y = y;                                          // config elem
+                    ((AbstractButtonWidget)this.buttons.get(i)).render(stack, mouseX, mouseY, partialTicks);
+                    ((AbstractButtonWidget)this.buttons.get(i+1)).y = y;                                        // reset button
+                    ((AbstractButtonWidget)this.buttons.get(i+1)).render(stack, mouseX, mouseY, partialTicks);
                 }
+                y += LINEHEIGHT;
             }
-            y+=LINEHEIGHT;
+
+            y = TOP_BAR_SIZE - scrollAmount;
+            for (String text: options) {
+                if (mouseX>this.width/2-155 && mouseX<this.width/2 && mouseY>y && mouseY<y+BUTTONHEIGHT) {
+                    StringRenderable tooltip=new TranslatableText(handler.getConfig().getTooltip(text));
+                    if (textRenderer.getWidth(tooltip)<=250) {
+                        renderTooltip(stack, tooltip, 0, mouseY);
+                    } else {
+                        List<StringRenderable> lines = textRenderer.wrapLines(tooltip, 250);
+                        renderTooltip(stack, lines, mouseX, mouseY);
+                    }
+                }
+                y+=LINEHEIGHT;
+            }
+
+            if (maxScroll > 0) {
+                // fill(stack, width-5, TOP_BAR_SIZE, width, height - BOTTOM_BAR_SIZE, 0xc0c0c0);
+                int pos = (int)((height - TOP_BAR_SIZE - BOTTOM_BAR_SIZE - BUTTONHEIGHT) * ((float)scrollAmount / maxScroll));
+                // fill(stack, width-5, pos, width, pos+BUTTONHEIGHT, 0x303030);
+                this.client.getTextureManager().bindTexture(WIDGETS_LOCATION);
+                drawTexture(stack, width-5, pos+TOP_BAR_SIZE, 0, 66, 4, 20);
+            }
+        } else {
+            colorSelector.render(stack, mouseX, mouseY, partialTicks);
         }
-        
         this.client.getTextureManager().bindTexture(DrawableHelper.BACKGROUND_TEXTURE);
         RenderSystem.disableDepthTest();
         drawTexture(stack, 0, 0, 0, 0, width, TOP_BAR_SIZE);
@@ -240,14 +278,6 @@ public class GuiModOptions extends Screen implements Supplier<Screen> {
 
         drawCenteredString(stack, textRenderer, screenTitle, this.width/2, (TOP_BAR_SIZE - textRenderer.fontHeight)/2, 0xffffff);
         ((AbstractButtonWidget)this.buttons.get(0)).render(stack, mouseX, mouseY, partialTicks);
-        
-        if (maxScroll > 0) {
-            // fill(stack, width-5, TOP_BAR_SIZE, width, height - BOTTOM_BAR_SIZE, 0xc0c0c0);
-            int pos = (int)((height - TOP_BAR_SIZE - BOTTOM_BAR_SIZE - BUTTONHEIGHT) * ((float)scrollAmount / maxScroll));
-            // fill(stack, width-5, pos, width, pos+BUTTONHEIGHT, 0x303030);
-            this.client.getTextureManager().bindTexture(WIDGETS_LOCATION);
-            drawTexture(stack, width-5, pos+TOP_BAR_SIZE, 0, 66, 4, 20);
-        }
     }
 
     @Override
@@ -283,6 +313,22 @@ public class GuiModOptions extends Screen implements Supplier<Screen> {
             return true;
         }
         return super.mouseDragged(mouseX, mouseY, button, deltaX, deltaY);
+    }
+    
+    private void enableColorSelector(String option, AbstractButtonWidget element) {
+        for (int i=1; i<buttons.size(); i++) {
+            buttons.get(i).visible = false;
+        }
+        colorSelector.setCurrentColor((Integer) handler.getConfig().getValue(option));
+        colorSelector.visible = true;
+        colorSelector.setLink(option, element);
+    }
+    
+    private void disableColorSelector() {
+        for (int i=1; i<buttons.size(); i++) {
+            buttons.get(i).visible = true;
+        }
+        colorSelector.visible = false;
     }
     
     private enum Type {INT, FLOAT, DOUBLE;}
@@ -406,6 +452,139 @@ public class GuiModOptions extends Screen implements Supplier<Screen> {
                 sliderValue=((Integer)value-min)/(max-min);
             }
             super.onFocusedChanged(b);
+        }
+    }
+    
+    private class ColorSelector extends AbstractButtonWidget {
+        
+        private ColorButton buttons[];
+        private int currentColor;
+        private String option;
+        private AbstractButtonWidget element;
+
+        private int standardColors[] = { 
+            0x000000, 0x0000AA, 0x00AA00, 0x00AAAA,
+            0xAA0000, 0xAA00AA, 0xFFAA00, 0xAAAAAA, 
+            0x555555, 0x5555FF, 0x55FF55, 0x55FFFF,
+            0xFF5555, 0xFF55FF, 0xFFFF55, 0xFFFFFF
+        };
+
+        public ColorSelector(Text message) {
+            super(0, 0, 120, 120, message);
+            buttons = new ColorButton[16];
+        }
+        
+        public void init() {
+            Text buttonText = new LiteralText("");
+            this.x = (GuiModOptions.this.width - width) / 2;
+            this.y = (GuiModOptions.this.height - height) / 2;
+            for (int i=0; i<16; i++) {
+                buttons[i]=new ColorButton(
+                    this, x + (i/4) * 25, y + (i%4)*25, 20, 20, buttonText, i, standardColors[i]
+                );
+            }
+            visible = false;
+        }
+        
+        @Override
+        public boolean mouseClicked(double mouseX, double mouseY, int button) {
+            if (visible) {
+                for (int i=0; i<buttons.length; i++) {
+                    if (buttons[i].mouseClicked(mouseX, mouseY, button))
+                        return true;
+                }
+            }
+            return false;
+        }
+        
+        @Override
+        public void render(MatrixStack stack, int mouseX, int mouseY, float partialTicks) {
+            if (visible) {
+                // renderButton(stack, mouseX, mouseY, partialTicks);
+                for (int i=0; i<16; i++) {
+                    buttons[i].render(stack, mouseX, mouseY, partialTicks);
+                }
+            }
+        }
+        
+        public void setLink(String option, AbstractButtonWidget element) {
+            this.option = option;
+            this.element = element;
+        }
+        
+        public void setCurrentColor(int color) {
+            currentColor = color;
+        }
+        
+        public int getCurrentColor() {
+            return currentColor;
+        }
+        
+        public void onColorSelected(int color) {
+            currentColor = color;
+            handler.onConfigChanging(new OnConfigChangingEvent(modName, option, color));
+            System.out.println("set "+option+" to "+color);
+            handler.getConfig().setValue(option, color);
+            element.setMessage(null);
+            disableColorSelector();
+        }
+    }
+    
+    private class ColorButton extends AbstractButtonWidget {
+        
+        private final ColorSelector parent;
+        private final int index;
+        private final int color;
+
+        public ColorButton(ColorSelector parent, int x, int y, int width, int height, Text message, int index, int color) {
+            super(x, y, width, height, message);
+            this.index = index;
+            this.color = color;
+            this.parent = parent;
+        }
+        
+        @Override
+        protected void renderBg(MatrixStack stack, MinecraftClient mc, int mouseX, int mouseY) {
+            if (this.visible) {
+                super.renderBg(stack, client, mouseX, mouseY);
+
+                final Tessellator tessellator = Tessellator.getInstance();
+                final BufferBuilder bufferBuilder = tessellator.getBuffer();
+                float red = ((color >> 16)/255.0f);
+                float green = (((color >> 8) & 0xff)/255.0f);
+                float blue = (((color >> 0) & 0xff) /255.0f);
+                
+                GlStateManager.disableTexture();
+
+                bufferBuilder.begin(GL11.GL_TRIANGLE_FAN, VertexFormats.POSITION_COLOR);
+                Matrix4f model = stack.peek().getModel();
+                int x1=this.x+3;
+                int x2=this.x+this.width-3;
+                int y1=this.y+3;
+                int y2=this.y+this.height-3;
+                if (index == parent.getCurrentColor()) {
+                    bufferBuilder.vertex(model, x1, y1, 0.0f).color(1.0f, 1.0f, 1.0f, 1.0f).next();
+                    bufferBuilder.vertex(model, x1, y2, 0.0f).color(1.0f, 1.0f, 1.0f, 1.0f).next();
+                    bufferBuilder.vertex(model, x2, y2, 0.0f).color(1.0f, 1.0f, 1.0f, 1.0f).next();
+                    bufferBuilder.vertex(model, x2, y1, 0.0f).color(1.0f, 1.0f, 1.0f, 1.0f).next();
+                    tessellator.draw();
+                    bufferBuilder.begin(GL11.GL_TRIANGLE_FAN, VertexFormats.POSITION_COLOR);
+                    x1++; y1++; x2--; y2--;
+                }
+                bufferBuilder.vertex(model, x1, y1, 0.0f).color(red, green, blue, 1.0f).next();
+                bufferBuilder.vertex(model, x1, y2, 0.0f).color(red, green, blue, 1.0f).next();
+                bufferBuilder.vertex(model, x2, y2, 0.0f).color(red, green, blue, 1.0f).next();
+                bufferBuilder.vertex(model, x2, y1, 0.0f).color(red, green, blue, 1.0f).next();
+                tessellator.draw();
+
+                GlStateManager.enableTexture();
+            }
+        }
+        
+        @Override
+        public void onClick(double mouseX, double mouseY) {
+            // System.out.println("selected "+Integer.toHexString(color)+" from button "+this.index);
+            parent.onColorSelected(this.index);
         }
     }
 }
