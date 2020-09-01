4a85703cd681590e4671bf6b0502a9324cd767e6
Guntram Blohm <gbl@guntram.de>
2020-09-01 09:34:43 +0200

    1.2.3; config gui scrolls now if gui scale too large

--- src/main/java/de/guntram/mcmod/fabrictools/GuiModOptions.java
+++ src/main/java/de/guntram/mcmod/fabrictools/GuiModOptions.java
@@ -1,12 +1,15 @@
 package de.guntram.mcmod.fabrictools;
 
 import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.systems.RenderSystem;
 import de.guntram.mcmod.fabrictools.ConfigChangedEvent.OnConfigChangingEvent;
 import java.util.List;
 import java.util.function.Supplier;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.AbstractButtonWidget;
+import static net.minecraft.client.gui.widget.AbstractButtonWidget.WIDGETS_LOCATION;
 import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.LiteralText;
@@ -31,10 +34,21 @@ public class GuiModOptions extends Screen implements Supplier<Screen> {
     private static final int LINEHEIGHT = 25;
     private static final int BUTTONHEIGHT = 20;
     
-    private boolean mouseReleased = false;
-    
-    private static final Text trueText = new TranslatableText("de.guntram.mcmod.fabrictools.true").copy().formatted(Formatting.GREEN);
-    private static final Text falseText = new TranslatableText("de.guntram.mcmod.fabrictools.false").copy().formatted(Formatting.RED);
+    //private static final Text trueText = new TranslatableText("de.guntram.mcmod.fabrictools.true").copy().formatted(Formatting.GREEN);
+    // private static final Text falseText = new TranslatableText("de.guntram.mcmod.fabrictools.false").copy().formatted(Formatting.RED);
+
+    private static final int TOP_BAR_SIZE = 40;
+    private static final int BOTTOM_BAR_SIZE = 35;
+
+    private boolean mouseReleased = false;          // used in sliders
+    private boolean isDraggingScrollbar = false;
+
+    private int buttonWidth;
+    private int scrollAmount;
+    private int maxScroll;
+
+    private static final Text trueText = new TranslatableText("de.guntram.mcmod.fabrictools.true").formatted(Formatting.GREEN);
+    private static final Text falseText = new TranslatableText("de.guntram.mcmod.fabrictools.false").formatted(Formatting.RED);
     
     @SuppressWarnings("OverridableMethodCallInConstructor")
     public GuiModOptions(Screen parent, String modName, ModConfigurationHandler confHandler) {
@@ -49,7 +63,11 @@ public class GuiModOptions extends Screen implements Supplier<Screen> {
     
     @Override
     protected void init() {
-        this.addButton(new AbstractButtonWidget(this.width / 2 - 100, this.height - 27, 200, 20, new TranslatableText("gui.done")) {
+        buttonWidth = this.width / 2 -50;
+        if (buttonWidth > 200) {
+            buttonWidth = 200;
+        }
+        this.addButton(new AbstractButtonWidget(this.width / 2 - 100, this.height - 27, 200, BUTTONHEIGHT, new TranslatableText("gui.done")) {
             @Override
             public void onClick(double x, double y) {
                 for (AbstractButtonWidget button: buttons) {
@@ -74,7 +92,7 @@ public class GuiModOptions extends Screen implements Supplier<Screen> {
                 continue;
             } else if (handler.getConfig().isSelectList(text)) {
                 String[] options = handler.getConfig().getListOptions(text);
-                element = this.addButton(new AbstractButtonWidget(this.width/2+10, y, 200, BUTTONHEIGHT, new TranslatableText(options[(Integer)value])) {
+                element = this.addButton(new AbstractButtonWidget(this.width/2+10, y, buttonWidth, BUTTONHEIGHT, new TranslatableText(options[(Integer)value])) {
                     @Override
                     public void onClick(double x, double y) {
                         int cur = (Integer) handler.getConfig().getValue(text);
@@ -93,7 +111,7 @@ public class GuiModOptions extends Screen implements Supplier<Screen> {
                     }
                 });
             } else if (value instanceof Boolean) {
-                element = this.addButton(new AbstractButtonWidget(this.width/2+10, y, 200, BUTTONHEIGHT, (Boolean) value == true ? trueText : falseText) {
+                element = this.addButton(new AbstractButtonWidget(this.width/2+10, y, buttonWidth, BUTTONHEIGHT, (Boolean) value == true ? trueText : falseText) {
                     @Override
                     public void onClick(double x, double y) {
                         if ((Boolean)(handler.getConfig().getValue(text))==true) {
@@ -112,7 +130,7 @@ public class GuiModOptions extends Screen implements Supplier<Screen> {
                     }
                 });
             } else if (value instanceof String) {
-                element=this.addButton(new TextFieldWidget(this.textRenderer, this.width/2+10, y, 200, BUTTONHEIGHT, new LiteralText((String) value)) {
+                element=this.addButton(new TextFieldWidget(this.textRenderer, this.width/2+10, y, buttonWidth, BUTTONHEIGHT, new LiteralText((String) value)) {
                     @Override
                     public void onFocusedChanged(boolean b) {
                         if (b) {
@@ -139,6 +157,7 @@ public class GuiModOptions extends Screen implements Supplier<Screen> {
                     }
                     
                 });
+                ((TextFieldWidget) element).setMaxLength(120);
                 element.changeFocus(false);
             } else if (value instanceof Integer || value instanceof Float || value instanceof Double) {
                 element=this.addButton(new GuiSlider(this.width/2+10, y, handler.getConfig(), text));
@@ -146,7 +165,7 @@ public class GuiModOptions extends Screen implements Supplier<Screen> {
                 LogManager.getLogger().warn(modName +" has option "+text+" with data type "+value.getClass().getName());
                 continue;
             }
-            this.addButton(new AbstractButtonWidget(this.width/2+220, y, BUTTONHEIGHT, BUTTONHEIGHT, new LiteralText("")) {
+            this.addButton(new AbstractButtonWidget(this.width/2+10+buttonWidth+10, y, BUTTONHEIGHT, BUTTONHEIGHT, new LiteralText("")) {
                 @Override
                 public void onClick(double x, double y) {
                     handler.getConfig().setValue(text, handler.getConfig().getDefault(text));
@@ -155,11 +174,28 @@ public class GuiModOptions extends Screen implements Supplier<Screen> {
                 }
             });
         }
+        maxScroll = (this.options.size())*LINEHEIGHT - (this.height - TOP_BAR_SIZE - BOTTOM_BAR_SIZE) + LINEHEIGHT;
+        if (maxScroll < 0) {
+            maxScroll = 0;
+        }
+    }
+    
+    @Override
+    public boolean mouseScrolled(double mouseX, double mouseY, double amount) {
+        this.scrollAmount = (this.scrollAmount - (int)(amount * BUTTONHEIGHT / 2));
+        if (scrollAmount < 0) {
+            scrollAmount = 0;
+        }
+        if (scrollAmount > maxScroll) {
+            scrollAmount = maxScroll;
+        }
+        return true;
     }
     
     @Override
     public void render(MatrixStack stack, int mouseX, int mouseY, float partialTicks) {
         renderBackground(stack);
+/*
         drawCenteredString(stack, textRenderer, screenTitle, this.width/2, BUTTONHEIGHT, 0xffffff);
         super.render(stack, mouseX, mouseY, partialTicks);
         
@@ -168,19 +204,49 @@ public class GuiModOptions extends Screen implements Supplier<Screen> {
             textRenderer.draw(stack, new TranslatableText(text), this.width / 2 -155, y+2, 0xffffff);
             y+=LINEHEIGHT;
         }
+*/
 
-        y=50;
+
+        int y = TOP_BAR_SIZE + LINEHEIGHT/2 - scrollAmount;
+        for (int i=1; i<this.buttons.size(); i+=2) {
+            if (y > TOP_BAR_SIZE - LINEHEIGHT/2 && y < height - BOTTOM_BAR_SIZE) {
+                textRenderer.draw(stack, new TranslatableText(options.get(i/2)), this.width / 2 -155, y+4, 0xffffff);
+                ((AbstractButtonWidget)this.buttons.get(i)).y = y;                                          // config elem
+                ((AbstractButtonWidget)this.buttons.get(i)).render(stack, mouseX, mouseY, partialTicks);
+                ((AbstractButtonWidget)this.buttons.get(i+1)).y = y;                                        // reset button
+                ((AbstractButtonWidget)this.buttons.get(i+1)).render(stack, mouseX, mouseY, partialTicks);
+            }
+            y += LINEHEIGHT;
+        }
+        
+        y = TOP_BAR_SIZE - scrollAmount;
         for (String text: options) {
             if (mouseX>this.width/2-155 && mouseX<this.width/2 && mouseY>y && mouseY<y+BUTTONHEIGHT) {
                 StringRenderable tooltip=new TranslatableText(handler.getConfig().getTooltip(text));
                 if (textRenderer.getWidth(tooltip)<=250) {
-                    renderTooltip(stack, tooltip, mouseX, mouseY);
+                    renderTooltip(stack, tooltip, 0, mouseY);
                 } else {
                     List<StringRenderable> lines = textRenderer.wrapLines(tooltip, 250);
                     renderTooltip(stack, lines, mouseX, mouseY);
                 }
             }
             y+=LINEHEIGHT;
+        }
+        
+        this.client.getTextureManager().bindTexture(DrawableHelper.BACKGROUND_TEXTURE);
+        RenderSystem.disableDepthTest();
+        drawTexture(stack, 0, 0, 0, 0, width, TOP_BAR_SIZE);
+        drawTexture(stack, 0, height-BOTTOM_BAR_SIZE, 0, 0, width, BOTTOM_BAR_SIZE);
+
+        drawCenteredString(stack, textRenderer, screenTitle, this.width/2, (TOP_BAR_SIZE - textRenderer.fontHeight)/2, 0xffffff);
+        ((AbstractButtonWidget)this.buttons.get(0)).render(stack, mouseX, mouseY, partialTicks);
+        
+        if (maxScroll > 0) {
+            // fill(stack, width-5, TOP_BAR_SIZE, width, height - BOTTOM_BAR_SIZE, 0xc0c0c0);
+            int pos = (int)((height - TOP_BAR_SIZE - BOTTOM_BAR_SIZE - BUTTONHEIGHT) * ((float)scrollAmount / maxScroll));
+            // fill(stack, width-5, pos, width, pos+BUTTONHEIGHT, 0x303030);
+            this.client.getTextureManager().bindTexture(WIDGETS_LOCATION);
+            drawTexture(stack, width-5, pos+TOP_BAR_SIZE, 0, 66, 4, 20);
         }
     }
 
@@ -191,13 +257,33 @@ public class GuiModOptions extends Screen implements Supplier<Screen> {
 
     @Override
     public boolean mouseReleased(double mouseX, double mouseY, int button) {
+        isDraggingScrollbar = false;
         if (button == 0) {
             mouseReleased = true;
         }
-        return super.mouseReleased(mouseX, mouseY, button); //To change body of generated methods, choose Tools | Templates.
+        return super.mouseReleased(mouseX, mouseY, button);
     }
     
+    @Override
+    public boolean mouseClicked(double mouseX, double mouseY, int button) {
+        if (button == 0 && maxScroll > 0 && mouseX > width-5) {
+            isDraggingScrollbar = true;
+            scrollAmount = (int)((mouseY - TOP_BAR_SIZE)/(height - BOTTOM_BAR_SIZE - TOP_BAR_SIZE)*maxScroll);
+            scrollAmount = MathHelper.clamp(scrollAmount, 0, maxScroll);
+            return true;
+        }
+        return super.mouseClicked(mouseX, mouseY, button);
+    }
     
+    @Override
+    public boolean mouseDragged(double mouseX, double mouseY, int button, double deltaX, double deltaY) {
+        if (isDraggingScrollbar) {
+            scrollAmount = (int)((mouseY - TOP_BAR_SIZE)/(height - BOTTOM_BAR_SIZE - TOP_BAR_SIZE)*maxScroll);
+            scrollAmount = MathHelper.clamp(scrollAmount, 0, maxScroll);
+            return true;
+        }
+        return super.mouseDragged(mouseX, mouseY, button, deltaX, deltaY);
+    }
     
     private enum Type {INT, FLOAT, DOUBLE;}
     
@@ -210,7 +296,7 @@ public class GuiModOptions extends Screen implements Supplier<Screen> {
         
         @SuppressWarnings("OverridableMethodCallInConstructor")
         GuiSlider(int x, int y, Configuration config, String option) {
-            super(x, y, 200, BUTTONHEIGHT, new LiteralText("?"));
+            super(x, y, buttonWidth, BUTTONHEIGHT, new LiteralText("?"));
             Object value=config.getValue(option);
             if (value instanceof Double) {
                 this.setMessage(new LiteralText(Double.toString((Double)value)));
